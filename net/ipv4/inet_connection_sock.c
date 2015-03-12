067141f4b704a7e4049b3b5a32284d3254d20a22
Eric Dumazet <edumazet@google.com>
2015-03-11 18:53:14 -0700

    BACKPORT [UPSTREAM] net: add real socket cookies
    Cherry-pick from 33cf7c90fe2f97afb1cadaa0cfb782cb9d1b9ee2. The socket
    cookie is used to identify the socket in side xt_qtaguid module. Using
    the socket cookie can release the socket reference count currently
    holding by qtaguid when the socket is tagged.
    
    A long standing problem in netlink socket dumps is the use
    of kernel socket addresses as cookies.
    
    1) It is a security concern.
    
    2) Sockets can be reused quite quickly, so there is
       no guarantee a cookie is used once and identify
       a flow.
    
    3) request sock, establish sock, and timewait socks
       for a given flow have different cookies.
    
    Part of our effort to bring better TCP statistics requires
    to switch to a different allocator.
    
    In this patch, I chose to use a per network namespace 64bit generator,
    and to use it only in the case a socket needs to be dumped to netlink.
    (This might be refined later if needed)
    
    Note that I tried to carry cookies from request sock, to establish sock,
    then timewait sockets.
    
    Bug: 36374484
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Eric Salo <salo@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Change-Id: Ib80b1eb1bf18b26df238808c4e495d53600348a8
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- net/ipv4/inet_connection_sock.c
+++ net/ipv4/inet_connection_sock.c
@@ -689,6 +689,8 @@ struct sock *inet_csk_clone_lock(const struct sock *sk,
 		inet_sk(newsk)->mc_list = NULL;
 
 		newsk->sk_mark = inet_rsk(req)->ir_mark;
+		atomic64_set(&newsk->sk_cookie,
+			     atomic64_read(&inet_rsk(req)->ir_cookie));
 
 		newicsk->icsk_retransmits = 0;
 		newicsk->icsk_backoff	  = 0;

2d6c3c1062b5446ec70509259e95a5ea6795a82a
James Morse <james.morse@arm.com>
2016-02-05 14:58:48 +0000

    BACKPORT: arm64: kernel: Add support for User Access Override
    'User Access Override' is a new ARMv8.2 feature which allows the
    unprivileged load and store instructions to be overridden to behave in
    the normal way.
    
    This patch converts {get,put}_user() and friends to use ldtr*/sttr*
    instructions - so that they can only access EL0 memory, then enables
    UAO when fs==KERNEL_DS so that these functions can access kernel memory.
    
    This allows user space's read/write permissions to be checked against the
    page tables, instead of testing addr<USER_DS, then using the kernel's
    read/write permissions.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    [catalin.marinas@arm.com: move uao_thread_switch() above dsb()]
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 31432001
    Change-Id: I2a916c187959d4eb11916d3719b198301ad26636
    (cherry picked from commit 57f4959bad0a154aeca125b7d38d1d9471a12422)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    
    Conflicts:
    	arch/arm64/include/asm/processor.h

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -170,6 +170,7 @@ static inline void spin_lock_prefetch(const void *x)
 #endif
 
 void cpu_enable_pan(void *__unused);
+void cpu_enable_uao(void *__unused);
 #include <asm-generic/processor.h>
 
 #endif /* __ASM_PROCESSOR_H */

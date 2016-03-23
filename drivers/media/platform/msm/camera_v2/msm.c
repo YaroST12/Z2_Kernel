d318a474caa1f70f280611a2112c45c307bc5211
Manish Poddar <mpoddar@codeaurora.org>
2016-03-23 16:57:23 +0530

    msm: camera: Fix KW errors.
    Add NULL check and initialize  the variables to
    fix KW errors in camera.
    
    CRs-fixed: 994317
    Change-Id: I165f1c39c1332737a644c6650bc40f4c718a7ee8
    Signed-off-by: Manish Poddar <mpoddar@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -710,6 +710,7 @@ static long msm_private_ioctl(struct file *file, void *fh,
 		return -ENOTTY;
 	}
 
+	memset(&event, 0, sizeof(struct v4l2_event));
 	session_id = event_data->session_id;
 	stream_id = event_data->stream_id;
 

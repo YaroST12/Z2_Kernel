f9bb6426d991bec902848c149ae46530eabfbb90
Jens Axboe <axboe@fb.com>
2016-12-02 20:00:14 -0700

    blk-mq: blk_account_io_start() takes a bool
    Signed-off-by: Jens Axboe <axboe@fb.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1122,7 +1122,7 @@ static void blk_mq_bio_to_request(struct request *rq, struct bio *bio)
 	init_request_from_bio(rq, bio);
 
 	if (blk_do_io_stat(rq))
-		blk_account_io_start(rq, 1);
+	    blk_account_io_start(rq, true);
 }
 
 static inline bool hctx_allow_merges(struct blk_mq_hw_ctx *hctx)

0bc4145f2b3e72f1f9ea2c0f86f5391812cb43fa
Alex Naidis <alex.naidis@linux.com>
2016-12-27 20:33:35 +0100

    Revert "USB: dwc3: msm: Kill tasklet before disabling irq in suspend"
    Blocks entering into LPM mode.
    
    This reverts commit 9a72294c603eeb2eaea028ada42d5800182f16ff.
    
    Change-Id: I69110c1fe3625c92afd6dcd7d2c8365a1cfaa332
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -3594,6 +3594,7 @@ static void dwc3_interrupt_bh(unsigned long param)
 {
 	struct dwc3 *dwc = (struct dwc3 *) param;
 
+	pm_runtime_get(dwc->dev);
 	dwc3_thread_interrupt(dwc->irq, dwc);
 	enable_irq(dwc->irq);
 }
@@ -3621,6 +3622,7 @@ static irqreturn_t dwc3_thread_interrupt(int irq, void *_dwc)
 	dwc->bh_completion_time[dwc->bh_dbg_index] = temp_time;
 	dwc->bh_dbg_index = (dwc->bh_dbg_index + 1) % 10;
 
+	pm_runtime_put(dwc->dev);
 	return ret;
 }
 

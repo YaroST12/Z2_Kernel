9a025756b803a64deb129c1a9219164c7586ae74
Todd Kjos <tkjos@google.com>
2017-02-15 08:25:58 -0800

    binder: fix unsafe access to binder thread
    No known cases have occurred, but thread->looper was being
    accessed at a point where thread might be NULL. Move down
    a few lines where it is safe.
    
    Bug: 32225111
    Change-Id: I5efdb9ebaec50c1f2326001cfaccc57afb82f356
    Test: tested manually
    Signed-off-by: Todd Kjos <tkjos@google.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2808,10 +2808,11 @@ retry:
 
 	if (wait_for_proc_work)
 		atomic_dec(&proc->ready_threads);
-	thread->looper &= ~BINDER_LOOPER_STATE_WAITING;
 
 	if (ret)
 		return ret;
+
+	thread->looper &= ~BINDER_LOOPER_STATE_WAITING;
 
 	while (1) {
 		uint32_t cmd;

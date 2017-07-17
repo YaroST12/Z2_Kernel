8c178deff5d4ecac85c38e2b99888f0aa53613b2
Jingxiang Ge <jge@codeaurora.org>
2017-07-17 11:37:55 +0800

    cnss: ignore pci suspend/resume when target is power down
    Ignore pci suspend/resume operation when target is power down.
    
    Change-Id: I805e34f27e31b17a1afb4e579aa56e9ee94f68ea
    CRs-Fixed: 2050280
    Signed-off-by: Jingxiang Ge <jge@codeaurora.org>

--- drivers/net/wireless/cnss/cnss_pci.c
+++ drivers/net/wireless/cnss/cnss_pci.c
@@ -1764,6 +1764,9 @@ static int cnss_wlan_pci_suspend(struct device *dev)
 	if (!penv)
 		goto out;
 
+	if (!penv->pcie_link_state)
+		goto out;
+
 	wdriver = penv->driver;
 	if (!wdriver)
 		goto out;
@@ -1789,6 +1792,9 @@ static int cnss_wlan_pci_resume(struct device *dev)
 	struct pci_dev *pdev = to_pci_dev(dev);
 
 	if (!penv)
+		goto out;
+
+	if (!penv->pcie_link_state)
 		goto out;
 
 	wdriver = penv->driver;

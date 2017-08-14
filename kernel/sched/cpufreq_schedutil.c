562f240d499d49f3fd751c6749e5aae898b9c177
YaroSt12 <yaro330@gmail.com>
2017-08-14 21:16:44 +0300

    schedutil: fix cherry-picking derp

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -27,6 +27,7 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
 #define SUGOV_KTHREAD_PRIORITY	50
+#define LATENCY_MULTIPLIER			(1000)
 
 struct sugov_tunables {
 	struct gov_attr_set attr_set;

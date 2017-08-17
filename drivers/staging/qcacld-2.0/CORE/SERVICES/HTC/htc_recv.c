c0890db203381c5a33fff601652e25f297729259
YaroSt12 <yaro330@gmail.com>
2017-08-17 14:43:34 +0300

    Revert "qcacld-2.0: fix some errors/remove code that causes error"
    This reverts commit 056a8d6b428049addb9d56224c8ced9a6aed7ae9.

--- drivers/staging/qcacld-2.0/CORE/SERVICES/HTC/htc_recv.c
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/HTC/htc_recv.c
@@ -57,7 +57,7 @@ void DebugDumpBytes(A_UCHAR *buffer, A_UINT16 length, char *pDescription)
         if(count == 16) {
             count = 0;
             offset = 0;
-            //A_SNPRINTF(byteOffsetStr, sizeof(byteOffset), "%4.4X",byteOffset);
+            A_SNPRINTF(byteOffsetStr, sizeof(byteOffset), "%4.4X",byteOffset);
             A_PRINTF("[%s]: %s\n", byteOffsetStr, stream);
             A_MEMZERO(stream, 60);
             byteOffset += 16;
@@ -65,7 +65,7 @@ void DebugDumpBytes(A_UCHAR *buffer, A_UINT16 length, char *pDescription)
     }
 
     if(offset != 0) {
-        //A_SNPRINTF(byteOffsetStr, sizeof(byteOffset), "%4.4X",byteOffset);
+        A_SNPRINTF(byteOffsetStr, sizeof(byteOffset), "%4.4X",byteOffset);
         A_PRINTF("[%s]: %s\n", byteOffsetStr, stream);
     }
 

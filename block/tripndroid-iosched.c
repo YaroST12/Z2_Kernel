17f7cc13a438608f9fe9e866c367b634ddd2a3bc
YaroSt12 <yaro330@gmail.com>
2017-08-30 20:42:33 +0300

    block: Update schedulers to work better on msm8996

--- block/tripndroid-iosched.c
+++ block/tripndroid-iosched.c
@@ -30,8 +30,8 @@ static const int sync_write_expire = 1 * HZ;	/* max time before a sync write is 
 static const int async_read_expire  =  2 * HZ;	/* ditto for async, these limits are SOFT! */
 static const int async_write_expire = 2 * HZ;	/* ditto for async, these limits are SOFT! */
 
-static const int writes_starved = 1;		/* max times reads can starve a write */
-static const int fifo_batch     = 1;		/* # of sequential requests treated as one
+static const int writes_starved = 16;		/* max times reads can starve a write */
+static const int fifo_batch     = 4;		/* # of sequential requests treated as one
 						   by the above parameters. For throughput. */
 
 struct tripndroid_data {

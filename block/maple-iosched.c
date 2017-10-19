acf0842502a67b2d1aa84f877b80f3bc5ed1d23d
YaroSt12 <yaro330@gmail.com>
2017-10-19 23:26:18 +0300

    Maple: tune presets a little bit
    Signed-off-by: YaroSt12 <yaro330@gmail.com>

--- block/maple-iosched.c
+++ block/maple-iosched.c
@@ -28,8 +28,8 @@ static const int sync_read_expire = 350;	/* max time before a read sync is submi
 static const int sync_write_expire = 550;	/* max time before a write sync is submitted. */
 static const int async_read_expire = 250;	/* ditto for read async, these limits are SOFT! */
 static const int async_write_expire = 450;	/* ditto for write async, these limits are SOFT! */
-static const int fifo_batch = 8;		/* # of sequential requests treated as one by the above parameters. */
-static const int writes_starved = 4;		/* max times reads can starve a write */
+static const int fifo_batch = 16;		/* # of sequential requests treated as one by the above parameters. */
+static const int writes_starved = 1;		/* max times reads can starve a write */
 static const int sleep_latency_multiple = 10;	/* multple for expire time when device is asleep */
 
 /* Elevator data */

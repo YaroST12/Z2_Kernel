b999490588bac7d838e21995d80736715a8a9a1c
YaroSt12 <yaro330@gmail.com>
2017-10-23 14:52:41 +0300

    Adapt for MSM8996 Allowing CPU-boost to boost all cores somehow murdered battery life, let's try this variant. Signed-off-by: YaroSt12 <yaro330@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -200,14 +200,14 @@ static void do_input_boost(struct work_struct *work)
 	for_each_possible_cpu(i) {
 		i_sync_info = &per_cpu(sync_info, i);
 
-		// cpu 0-3 -> silver cluster
-		// cpu 4-7 -> gold cluster
+		// cpu 0-1 -> silver cluster
+		// cpu 2-3 -> gold cluster
 		// to save power there's no point in boosting the
 		// gold cluster core if it doesn't have any runnable
 		// thread at this point in time
 		// since inputs are fairly common we might save some
 		// juice in the long run
-		if (i >= 4 && cpu_rq(i)->nr_running == 0)
+		if (i >= 1 && cpu_rq(i)->nr_running == 0)
 			continue;
 
 		i_sync_info->input_boost_min = i_sync_info->input_boost_freq;

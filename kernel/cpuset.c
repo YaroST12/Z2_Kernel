047c6ced029fe428e74c78062a3112ca234e6b39
YaroSt12 <yaro330@gmail.com>
2017-10-28 19:55:19 +0300

    Revert "sched/cpuset/pm: Fix cpuset vs. suspend-resume bugs"
    This reverts commit f957083d01914ca97627ea299ee70b7cf3884eb8.

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2263,13 +2263,6 @@ retry:
 	mutex_unlock(&cpuset_mutex);
 }
 
-static bool force_rebuild;
-
-void cpuset_force_rebuild(void)
-{
-	force_rebuild = true;
-}
-
 /**
  * cpuset_hotplug_workfn - handle CPU/memory hotunplug for a cpuset
  *
@@ -2344,10 +2337,8 @@ static void cpuset_hotplug_workfn(struct work_struct *work)
 	}
 
 	/* rebuild sched domains if cpus_allowed has changed */
-	if (cpus_updated || force_rebuild) {
-		force_rebuild = false;
+	if (cpus_updated)
 		rebuild_sched_domains();
-	}
 }
 
 void cpuset_update_active_cpus(bool cpu_online)
@@ -2364,11 +2355,6 @@ void cpuset_update_active_cpus(bool cpu_online)
 	 */
 	partition_sched_domains(1, NULL, NULL);
 	schedule_work(&cpuset_hotplug_work);
-}
-
-void cpuset_wait_for_hotplug(void)
-{
-	flush_work(&cpuset_hotplug_work);
 }
 
 /*

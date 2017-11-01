5364326a2d4b1260f17eac0f0b52fccfde12aaee
YaroSt12 <yaro330@gmail.com>
2017-11-01 15:12:04 +0300

    msm: mdss: avoid scheduling pp work during continuous updates Cherry-picked from https://github.com/AOSPA/android_kernel_leeco_msm8996/commit/b37f4af1af53f676a108437594ddf05b62bacc66 Signed-off-by: YaroSt12 <yaro330@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -1111,6 +1111,7 @@ static void mdss_mdp_cmd_pingpong_done(void *arg)
 			       atomic_read(&ctx->koff_cnt));
 		if (sync_ppdone) {
 			atomic_inc(&ctx->pp_done_cnt);
+		if (!ctl->commit_in_progress)
 			schedule_work(&ctx->pp_done_work);
 
 			mdss_mdp_resource_control(ctl,

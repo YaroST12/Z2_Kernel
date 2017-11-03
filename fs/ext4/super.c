42c544f53a03f2e57aad66c8274ebcca63d5f722
Joe Maples <joe@frap129.org>
2017-11-03 09:59:59 -0400

    kernel/fs/mm/block/drivers/scripts/net: More tautological comparison fixes
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: YaroST12 <yaro330@gmail.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -2417,9 +2417,6 @@ static loff_t ext4_max_bitmap_size(int bits, int has_huge_files)
 	if (res > upper_limit)
 		res = upper_limit;
 
-	if (res > MAX_LFS_FILESIZE)
-		res = MAX_LFS_FILESIZE;
-
 	return res;
 }
 

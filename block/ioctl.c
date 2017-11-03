4181349b53811b2b3db77fc45c255b8f2b777455
Joe Maples <joe@frap129.org>
2017-11-03 09:59:59 -0400

    kernel/fs/mm/block/drivers/scripts/net: More tautological comparison fixes
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: YaroST12 <yaro330@gmail.com>

--- block/ioctl.c
+++ block/ioctl.c
@@ -411,8 +411,6 @@ int blkdev_ioctl(struct block_device *bdev, fmode_t mode, unsigned cmd,
 		break;
 	case BLKGETSIZE:
 		size = i_size_read(bdev->bd_inode);
-		if ((size >> 9) > ~0UL)
-			return -EFBIG;
 		return put_ulong(arg, size >> 9);
 	case BLKGETSIZE64:
 		return put_u64(arg, i_size_read(bdev->bd_inode));

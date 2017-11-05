7a92e47c53390c1493c7ce0faeb2a5e4d68611c8
YaroSt12 <yaro330@gmail.com>
2017-11-05 15:56:16 +0300

    fpc1020_ree: let's try yet another variant of WQ
    Signed-off-by: YaroSt12 <yaro330@gmail.com>

--- drivers/input/misc/fpc1020_ree.c
+++ drivers/input/misc/fpc1020_ree.c
@@ -423,7 +423,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 		goto error_remove_sysfs;
 	}
 	
-	fpc1020->fpc1020_wq = create_workqueue("fpc1020_wq");
+	fpc1020->fpc1020_wq = alloc_workqueue("fpc1020_wq", WQ_UNBOUND | WQ_POWER_EFFICIENT, 1);
 	if (!fpc1020->fpc1020_wq) {
 		pr_err("Create input workqueue failed\n");
 		goto error_unregister_device;

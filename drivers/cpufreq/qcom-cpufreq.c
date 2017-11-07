d3b155ecb24b3aab36b2fc28a0caffa4a128aa0e
YaroSt12 <yaro330@gmail.com>
2017-11-07 12:58:21 +0300

    qcom-cpufreq: underclock: change max frequencies a bit I just like this setup more. Signed-off-by: YaroSt12 <yaro330@gmail.com>

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -337,8 +337,8 @@ static struct cpufreq_driver msm_cpufreq_driver = {
  * TODO: Investigate why this happens and find a proper fix that allows use of
  * all stock frequencies.
 */
-#define UNDERCLOCKED_MAX_KHZ_PERFCL	1824000
-#define UNDERCLOCKED_MAX_KHZ_PWRCL	1478400
+#define UNDERCLOCKED_MAX_KHZ_PERFCL	1996800
+#define UNDERCLOCKED_MAX_KHZ_PWRCL	1401600
 static bool no_cpu_underclock;
 
 static int __init get_cpu_underclock(char *unused)

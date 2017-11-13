29681c32ca5dcd7a385b25a4f8080161e42ff78b
YaroSt12 <yaro330@gmail.com>
2017-11-13 18:04:15 +0300

    sched\fair: fix tiny warning kernel/sched/fair.c:10225:21: warning: initialization from incompatible pointer type [-Wincompatible-pointer-types]   .migrate_task_rq = migrate_task_rq_fair, Signed-off-by: YaroSt12 <yaro330@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6583,7 +6583,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
  * cfs_rq_of(p) references at time of call are still valid and identify the
  * previous cpu. The caller guarantees p->pi_lock or task_rq(p)->lock is held.
  */
-static void migrate_task_rq_fair(struct task_struct *p, int next_cpu)
+static void migrate_task_rq_fair(struct task_struct *p)
 {
 	/*
 	 * We are supposed to update the task to "current" time, then its up to date

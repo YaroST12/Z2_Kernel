4ec33078be2bf318209d9e38380689340f48804d
YaroSt12 <yaro330@gmail.com>
2017-11-23 22:02:28 +0300

    boeffla_wl_blocker: [timerfd] stfu
    Signed-off-by: YaroSt12 <yaro330@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT			"wcnss_filter_lock;wlan;wlan_ipa;IPA_WS;wlan_pno_wl;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK"
+#define LIST_WL_DEFAULT			"wcnss_filter_lock;wlan;wlan_ipa;IPA_WS;wlan_pno_wl;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;[timerfd]"
 
 #define LENGTH_LIST_WL			255
 #define LENGTH_LIST_WL_DEFAULT		250

e13d39bb6d4b4afc3c35863bf46f0b2d34b0af0f
YaroSt12 <yaro330@gmail.com>
2017-11-28 19:28:32 +0300

    Revert "mm: compaction: compact all zones when suspended"
    This reverts commit 8e219741f8a7e418522083bd54fee6976f9864b5.

--- mm/compaction.c
+++ mm/compaction.c
@@ -17,9 +17,6 @@
 #include <linux/balloon_compaction.h>
 #include <linux/page-isolation.h>
 #include <linux/kasan.h>
-#ifdef CONFIG_STATE_NOTIFIER
-#include <linux/state_notifier.h>
-#endif
 #include "internal.h"
 
 #ifdef CONFIG_COMPACTION
@@ -1466,28 +1463,6 @@ break_loop:
 	return rc;
 }
 
-#ifdef CONFIG_STATE_NOTIFIER
-static void compact_nodes(void);
-
-static int state_notifier_callback(struct notifier_block *this,
-				unsigned long event, void *data)
-{
-	switch (event) {
-		case STATE_NOTIFIER_SUSPEND:
-			compact_nodes();
-			break;
-		default:
-			break;
-	}
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block compact_notifier_block = {
-	.notifier_call = state_notifier_callback,
-	.priority = -1,
-};
-#endif
 
 /* Compact all zones within a node */
 static void __compact_pgdat(pg_data_t *pgdat, struct compact_control *cc)
@@ -1607,12 +1582,4 @@ void compaction_unregister_node(struct node *node)
 }
 #endif /* CONFIG_SYSFS && CONFIG_NUMA */
 
-#ifdef CONFIG_STATE_NOTIFIER
-static int  __init mem_compaction_init(void)
-{
-	state_register_client(&compact_notifier_block);
-	return 0;
-}
-late_initcall(mem_compaction_init);
-#endif
 #endif /* CONFIG_COMPACTION */

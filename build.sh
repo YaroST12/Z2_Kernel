d701f6c34a812342750068c73c31cdd5097e6db6
YaroSt12 <yaro330@gmail.com>
2017-11-30 23:09:39 +0300

    build script update
    Signed-off-by: YaroSt12 <yaro330@gmail.com>

--- build.sh
+++ build.sh
@@ -9,19 +9,18 @@ export PATH=$PATH:${TOOL_CHAIN_PATH}
 export objdir="${kernel_dir}/../out"
 cd $kernel_dir
 compile() {
-  make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE}  $CONFIG_FILE -j8
-  make O=$objdir -j8
+	make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE} $CONFIG_FILE -j8
+	make O=$objdir -j8
 }
 ramdisk() {
-cp ramdisk/init.qcom.power.rc ../build/ramdisk/
-cd ${objdir}
-cp arch/arm64/boot/Image.gz-dtb ../build/Image.gz-dtb
+	cp ramdisk/init.qcom.power.rc ../build/ramdisk/
+	cd ${objdir}
+	cp arch/arm64/boot/Image.gz-dtb ../build/Image.gz-dtb
 }
 dtbuild(){
-  cd $kernel_dir
-  ./tools/dtbToolCM -2 -o $objdir/arch/arm64/boot/dt.img -s 4096 -p $objdir/scripts/dtc/ $objdir/arch/arm64/boot/dts/
+	cd $kernel_dir
+	./tools/dtbToolCM -2 -o $objdir/arch/arm64/boot/dt.img -s 4096 -p $objdir/scripts/dtc/ $objdir/arch/arm64/boot/dts/
 }
 compile 
 ramdisk
-cd ${objdir}
 cd ${kernel_dir}

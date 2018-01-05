aa4f5cc4187f09bb073376c80fa4de3c4735d323
YaroSt12 <yaro330@gmail.com>
2018-01-05 20:34:12 +0300

    cpufreq: schedutil: add exact cpu variable to sugov_cpu This includes https://github.com/torvalds/linux/commit/d62d813c0d714a2d0aaf3d796a7a51ae60bf5470#diff-d96c15a8e1af1ce4aa4750bef6b7e38d commit and fixes https://github.com/torvalds/linux/commit/466a2b42d67644447a1765276259a3ea5531ddff#diff-d96c15a8e1af1ce4aa4750bef6b7e38d. Signed-off-by: YaroSt12 <yaro330@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -74,6 +74,7 @@ struct sugov_policy {
 struct sugov_cpu {
 	struct update_util_data update_util;
 	struct sugov_policy *sg_policy;
+	unsigned int cpu;
 
 	bool iowait_boost_pending;
 	unsigned long iowait_boost;
@@ -885,6 +886,7 @@ static int sugov_start(struct cpufreq_policy *policy)
 		struct sugov_cpu *sg_cpu = &per_cpu(sugov_cpu, cpu);
 
 		memset(sg_cpu, 0, sizeof(*sg_cpu));
+		sg_cpu->cpu = cpu;
 		sg_cpu->sg_policy = sg_policy;
 		sg_cpu->flags = SCHED_CPUFREQ_DL;
 		sg_cpu->iowait_boost_max = policy->cpuinfo.max_freq;

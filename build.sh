e65b9a21ed94dfdfd7661c8fbb43cc68d20a74fe
YaroST12 <yaro330@gmail.com>
2018-01-21 02:46:46 +0300

    Update build script
    Signed-off-by: YaroST12 <yaro330@gmail.com>

--- build.sh
+++ build.sh
@@ -9,20 +9,18 @@ export CROSS_COMPILE="aarch64-linaro-linux-android-"
 export TOOL_CHAIN_PATH="${HOME}/build/z2/aarch64-linaro-7.x/bin"
 export LD_LIBRARY_PATH="${TOOL_CHAIN_PATH}/../lib"
 export PATH=$PATH:${TOOL_CHAIN_PATH}
-export objdir="${kernel_dir}/../out"
+export objdir="${kernel_dir}/out"
 export builddir="${kernel_dir}/build"
 cd $kernel_dir
-compile() {
+compile() 
+{
 	make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE} $CONFIG_FILE -j8
 	make O=$objdir -j8
 }
-ramdisk() {
+ramdisk() 
+{
 	cd ${objdir}
 	cp arch/arm64/boot/Image.gz-dtb $builddir/Image.gz-dtb
-}
-dtbuild(){
-	cd $kernel_dir
-	./tools/dtbToolCM -2 -o $objdir/arch/arm64/boot/dt.img -s 4096 -p $objdir/scripts/dtc/ $objdir/arch/arm64/boot/dts/
 }
 compile 
 ramdisk

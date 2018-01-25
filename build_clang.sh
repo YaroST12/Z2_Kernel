250c9e56a14af25cd036f3192c848f0a070008ab
YaroST12 <yaro330@gmail.com>
2018-01-25 14:46:53 +0300

    Update clang build script And gitignore. Signed-off-by: YaroST12 <yaro330@gmail.com>

--- build_clang.sh
+++ build_clang.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 kernel_dir=$PWD
 export CONFIG_FILE="z2_plus_defconfig"
-export ARCH="arm64"
+export ARCH=arm64
+export SUBARCH=arm64
 export KBUILD_BUILD_HOST="CryptoCountry"
 export KBUILD_BUILD_USER="Yaro330"
 export LOCALVERSION="-FokkenLazerSight"
-export CROSS_COMPILE="aarch64-linux-gnu-"
-export TOOL_CHAIN_PATH="${HOME}/build/z2/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin"
+export CLANG_TRIPLE="aarch64-linux-gnu-"
+export CROSS_COMPILE="${HOME}/build/z2/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-"
 export CLANG_TCHAIN="${HOME}/build/z2/linux-x86/clang-4536805/bin/clang"
 export LD_LIBRARY_PATH="${TOOL_CHAIN_PATH}/../lib"
 export PATH=$PATH:${TOOL_CHAIN_PATH}
-export objdir="${kernel_dir}/out"
+export objdir="${kernel_dir}/out_clang"
 export builddir="${kernel_dir}/build"
 cd $kernel_dir
 make_a_fucking_defconfig() 
@@ -19,11 +20,7 @@ make_a_fucking_defconfig()
 }
 compile() 
 {
-	PATH=${BIN_FOLDER}:${PATH} make O=$objdir -j8 \
-	CC="${CLANG_TCHAIN}" \
-	CLANG_TRIPLE=aarch64-linux-gnu- \
-	CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE} \
-	HOSTCC="${CLANG_TCHAIN}"
+	make CC="${CLANG_TCHAIN}" O=$objdir -j8 Image.gz-dtb
 }
 ramdisk() 
 {

af4972dcc3dfa5c7212c75d0ebdd5990330d84bb
YaroST12 <yaro330@gmail.com>
2018-01-26 15:15:41 +0300

    camera_v2: import flashlight control and some edits from ZUK Striped version of https://github.com/YaroST12/Z2_PLUS_Kernel_EAS/commit/8d54fad0d22bd33ebebbbbed862b9dbaf4a1ff7a#diff-d6c6541a68f1c7fd85ae0c09f21e66d8 Signed-off-by: YaroST12 <yaro330@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_cci_i2c.c
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_cci_i2c.c
@@ -394,8 +394,10 @@ int32_t msm_camera_cci_i2c_poll(struct msm_camera_i2c_client *client,
 	uint32_t addr, uint16_t data,
 	enum msm_camera_i2c_data_type data_type, uint32_t delay_ms)
 {
-	int32_t rc;
+	int32_t rc = -EFAULT;
+#if 0
 	int32_t i = 0;
+#endif
 	S_I2C_DBG("%s: addr: 0x%x data: 0x%x dt: %d\n",
 		__func__, addr, data, data_type);
 
@@ -404,6 +406,7 @@ int32_t msm_camera_cci_i2c_poll(struct msm_camera_i2c_client *client,
 			__func__, __LINE__, delay_ms, MAX_POLL_DELAY_MS);
 		return -EINVAL;
 	}
+#if 0
 	for (i = 0; i < delay_ms; i++) {
 		rc = msm_camera_cci_i2c_compare(client,
 			addr, data, data_type);
@@ -411,7 +414,19 @@ int32_t msm_camera_cci_i2c_poll(struct msm_camera_i2c_client *client,
 			return rc;
 		usleep_range(1000, 1010);
 	}
+#else
+	do {
+		rc = msm_camera_cci_i2c_compare(client,
+			addr, data, data_type);
+		if (rc == I2C_COMPARE_MATCH || rc < 0)
+			return rc;
 
+		if(delay_ms == 0)
+			break;
+
+		usleep_range(1000, 1010);
+	} while(delay_ms-- > 0);
+#endif
 	/* If rc is 1 then read is successful but poll is failure */
 	if (rc == 1)
 		pr_err("%s:%d poll failed rc=%d(non-fatal)\n",

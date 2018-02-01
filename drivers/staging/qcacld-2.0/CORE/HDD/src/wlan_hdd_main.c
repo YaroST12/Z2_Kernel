73cb064880b9239f8c92354053da007c7891c4c0
YaroST12 <yaro330@gmail.com>
2018-02-01 10:47:41 +0300

    qcacld-2.0: add back ifdefs around rw_wakelock
    What the actual fuck CAF, what the actual fuck?
    Why was it removed? WHY? 20k wakeups per hour, FUCKING THANK YOU.
    
    Signed-off-by: YaroST12 <yaro330@gmail.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
@@ -14779,8 +14779,10 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
          "%s: Failed to close VOSS Scheduler",__func__);
       VOS_ASSERT( VOS_IS_STATUS_SUCCESS( vosStatus ) );
    }
+#ifdef CONFIG_WLAN_FEATURE_RX_WAKELOCK
    /* Destroy the wake lock */
    vos_wake_lock_destroy(&pHddCtx->rx_wake_lock);
+#endif
    /* Destroy the wake lock */
    vos_wake_lock_destroy(&pHddCtx->sap_wake_lock);
 
@@ -15116,7 +15118,9 @@ void hdd_wlan_wakelock_destroy(void)
 void wlan_hdd_wakelocks_destroy(hdd_context_t *hdd_ctx)
 {
 	if (hdd_ctx) {
+#ifdef CONFIG_WLAN_FEATURE_RX_WAKELOCK
 		vos_wake_lock_destroy(&hdd_ctx->rx_wake_lock);
+#endif
 		vos_wake_lock_destroy(&hdd_ctx->sap_wake_lock);
 		hdd_hostapd_channel_wakelock_deinit(hdd_ctx);
 	}
@@ -17319,10 +17323,11 @@ int hdd_wlan_startup(struct device *dev, v_VOID_t *hif_sc)
    }
    reg_netdev_notifier_done = TRUE;
 #endif
-
+#ifdef CONFIG_WLAN_FEATURE_RX_WAKELOCK
    /* Initialize the wake lcok */
    vos_wake_lock_init(&pHddCtx->rx_wake_lock,
            "qcom_rx_wakelock");
+#endif
    /* Initialize the wake lcok */
    vos_wake_lock_init(&pHddCtx->sap_wake_lock,
            "qcom_sap_wakelock");

ad045ca6259f908014194ad8c8a351c86d206f85
YaroST12 <yaro330@gmail.com>
2018-02-03 17:29:07 +0300

    Build scripts: use Clang as default now
    Signed-off-by: YaroST12 <yaro330@gmail.com>

--- build.sh
+++ build.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 kernel_dir=$PWD
 export CONFIG_FILE="z2_plus_defconfig"
-export ARCH="arm64"
+export ARCH=arm64
+export SUBARCH=arm64
 export KBUILD_BUILD_HOST="CryptoCountry"
 export KBUILD_BUILD_USER="Yaro330"
 export LOCALVERSION="-FokkenLazerSight"
-export CROSS_COMPILE="aarch64-linaro-linux-android-"
-export TOOL_CHAIN_PATH="${HOME}/build/z2/aarch64-linaro-7.x/bin"
+export CLANG_TRIPLE="aarch64-linux-gnu-"
+export CROSS_COMPILE="${HOME}/build/z2/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-"
+export CLANG_TCHAIN="${HOME}/build/z2/linux-x86/clang-4536805/bin/clang"
 export LD_LIBRARY_PATH="${TOOL_CHAIN_PATH}/../lib"
 export PATH=$PATH:${TOOL_CHAIN_PATH}
-export objdir="${kernel_dir}/../out"
+export objdir="${kernel_dir}/out"
 export builddir="${kernel_dir}/build"
 cd $kernel_dir
-compile() {
-	make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE} $CONFIG_FILE -j8
-	make O=$objdir -j8
+make_a_fucking_defconfig() 
+{
+	make ARCH="arm64" O=$objdir $CONFIG_FILE -j8
 }
-ramdisk() {
+compile() 
+{
+	make CC="${CLANG_TCHAIN}" O=$objdir -j8 Image.gz-dtb
+}
+ramdisk() 
+{
 	cd ${objdir}
 	cp arch/arm64/boot/Image.gz-dtb $builddir/Image.gz-dtb
 }
-dtbuild(){
-	cd $kernel_dir
-	./tools/dtbToolCM -2 -o $objdir/arch/arm64/boot/dt.img -s 4096 -p $objdir/scripts/dtc/ $objdir/arch/arm64/boot/dts/
-}
+make_a_fucking_defconfig
 compile 
 ramdisk
 cd ${kernel_dir}

7dd6a10dd86ed020947d15e39232c64e7ad5f2f5
YaroST12 <yaro330@gmail.com>
2018-03-16 01:08:31 +0300

    algif_skcipher: remove an empty line For a cleaner merge. Signed-off-by: YaroST12 <yaro330@gmail.com>

--- crypto/algif_skcipher.c
+++ crypto/algif_skcipher.c
@@ -448,7 +448,6 @@ static int skcipher_recvmsg(struct kiocb *unused, struct socket *sock,
 		char __user *from = iov->iov_base;
 
 		while (seglen) {
-
 			used = ctx->used;
 			if (!used) {
 				err = skcipher_wait_for_data(sk, flags);

cfb659f2dfd57d2af62ac4b3979494a212ff0efc
Yaroslav Furman <yaro330@gmail.com>
2018-11-08 23:26:59 +0300

    fixup! kernel: Boost all CPUs to the max when userspace launches an app
    I fucked up conflict resolution in https://github.com/YaroST12/Z2_Kernel/commit/4239fb4b00f5083e194847102dd12e83206d13d8
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1646,10 +1646,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
-		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
-			atomic_set(&zygote32_pid, current->pid);
-		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
-			atomic_set(&zygote64_pid, current->pid);
+	if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
+		atomic_set(&zygote32_pid, current->pid);
+	else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
+		atomic_set(&zygote64_pid, current->pid);
 
 	/* execve succeeded */
 	current->fs->in_exec = 0;

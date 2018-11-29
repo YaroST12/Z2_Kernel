1302f51b46fcb3691b6b617f4236b902107e7c67
Yaroslav Furman <yaro330@gmail.com>
2018-11-29 19:15:13 +0300

    qpnp-fg: don't bail out when debugfs enabled
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg.c
+++ drivers/power/supply/qcom/qpnp-fg.c
@@ -9013,6 +9013,7 @@ static int fg_probe(struct platform_device *pdev)
 	 */
 	chip->batt_psy_name = "battery";
 
+#ifdef CONFIG_DEBUG_FS
 	if (chip->mem_base) {
 		rc = fg_dfs_create(chip);
 		if (rc < 0) {
@@ -9020,6 +9021,7 @@ static int fg_probe(struct platform_device *pdev)
 			goto power_supply_unregister;
 		}
 	}
+#endif
 
 	/* Fake temperature till the actual temperature is read */
 	chip->last_good_temp = 250;
@@ -9038,8 +9040,10 @@ static int fg_probe(struct platform_device *pdev)
 
 	return rc;
 
+#ifdef CONFIG_DEBUG_FS
 power_supply_unregister:
 	power_supply_unregister(chip->bms_psy);
+#endif
 cancel_work:
 	fg_cancel_all_works(chip);
 of_init_fail:

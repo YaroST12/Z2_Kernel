c9d05aa5e69213171955cf09be13751cf2851450
Yaroslav Furman <yaro330@gmail.com>
2019-01-24 14:41:58 +0300

    fpc1020_ree: use system wq for suspend/resume work
    There sin't really any reason to use a high priority wq for that.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/fingerprint/fpc1020_ree.c
+++ drivers/fingerprint/fpc1020_ree.c
@@ -433,11 +433,11 @@ static int fb_notifier_callback(struct notifier_block *self,
 		if (*blank == FB_BLANK_UNBLANK) {
 			pr_debug("ScreenOn\n");
 			fpc1020->screen_on = 1;
-			queue_work(fpc1020->fpc1020_wq, &fpc1020->pm_work);
+			schedule_work(&fpc1020->pm_work);
 		} else if (*blank == FB_BLANK_POWERDOWN) {
 			pr_debug("ScreenOff\n");
 			fpc1020->screen_on = 0;
-			queue_work(fpc1020->fpc1020_wq, &fpc1020->pm_work);
+			schedule_work(&fpc1020->pm_work);
 		}
 	}
 	return 0;

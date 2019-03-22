39a637cdf097aee63b9442049fb46d32294ecc93
Yaroslav Furman <yaro330@gmail.com>
2019-03-22 22:14:29 +0300

    mdss_fb: Boost stune when a new frame is ready to be committed
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -48,6 +48,7 @@
 #include <linux/dma-buf.h>
 #include <sync.h>
 #include <sw_sync.h>
+#include "../../../../kernel/sched/tune.h"
 
 #include "mdss_fb.h"
 #include "mdss_mdp_splash_logo.h"
@@ -5087,6 +5088,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+		ta_dyn_boost_kick();
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 

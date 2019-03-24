34e19fc005533530760ef0dd90e555d4c8b50b53
Yaroslav Furman <yaro330@gmail.com>
2019-03-24 17:33:04 +0300

    sched: tune: adjust stune boost/prefer_idle values
    1. Bump up bg tasks to -30 because -100 seens like a bit too much.
    2. Allow fg tasks to use idle CPUs and set boost to 0.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -699,9 +699,9 @@ static char *stune_groups[] = {
 	"rt",
 };
 
-static const int stune_values[] = { 5, -10, -100, 0 };
+static const int stune_values[] = { 5, 0, -30, 0 };
 
-static const bool prefer_idle_values[] = { 1, 0, 0, 0 };
+static const bool prefer_idle_values[] = { 1, 1, 0, 0 };
 
 static int boost_write_wrapper(struct cgroup_subsys_state *css,
 			struct cftype *cft, s64 boost)

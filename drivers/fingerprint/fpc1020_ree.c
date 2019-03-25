67297ef824f0f9e769a11efc477ef87b20524625
Yaroslav Furman <yaro330@gmail.com>
2019-03-26 00:56:53 +0300

    fpc1020_ree: enable screen on/off logger back
    Apparently we disabled all of them but having at least 1 is kinda nice
    for debug purposes.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/fingerprint/fpc1020_ree.c
+++ drivers/fingerprint/fpc1020_ree.c
@@ -416,11 +416,11 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK && fpc1020) {
 		blank = evdata->data;
 		if (*blank == FB_BLANK_UNBLANK) {
-			pr_debug("ScreenOn\n");
+			pr_info("ScreenOn\n");
 			fpc1020->screen_on = 1;
 			queue_work(fpc1020->fpc1020_wq, &fpc1020->pm_work);
 		} else if (*blank == FB_BLANK_POWERDOWN) {
-			pr_debug("ScreenOff\n");
+			pr_info("ScreenOff\n");
 			fpc1020->screen_on = 0;
 			queue_work(fpc1020->fpc1020_wq, &fpc1020->pm_work);
 		}
